60d800223d62f0c9f43b42043908f78193b02c30
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-01-24 20:39:13 -0300

    migration criada e controller para usuário parcialmente implementado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,23 @@
 const AppError = require("../utils/appError");
+const knex = require("../database/knex");
+const {hash} = require("bcryptjs");
 
 class UserController{
 
-  create(request, response){
+  async create(request, response){
     const {name, email, password} = request.body;
 
-    if(!name){
-      throw new AppError("Nome não informado!");
+    const checkUserExists = await knex("users").where({email}).first();
+
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está em uso!");
     }
+
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({name, email, password: hashedPassword});
     
-    return response.json( {name, email, password} )
+    return response.status(201).json();
   }
 
 }
