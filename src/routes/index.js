523b6538351c8b5b36a6bf642232c9dd996e0eb9
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-01-25 14:54:01 -0300

    Controller de session  implementado

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const {Router} = require("express");
 const routes = Router();
 
 const usersRouter = require("./user.routes");
+const sessionsRouter = require("./sessions.routes");
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
