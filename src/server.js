665c0d025d11f3c183066b312c5c3b5d37a239b5
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-02-26 14:32:46 -0300

    Variáveis de ambiente configuradas e pm2 configurado

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 const AppError = require("./utils/appError");
 const express = require("express");
 const routes = require("./routes");
@@ -33,7 +34,7 @@ app.use( (error, request, response, next) =>{
 
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 })
